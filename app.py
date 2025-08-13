5e1f542585f105f3811b5837bf2e658407627994
tr-sbabu <contact.trsathishbabu@gmail.com>
2025-08-13 11:28:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ if st.button("Calculate"):
         if num2 != 0:
             result = num1 / num2
         else:
-            result = "Error: Division by zero!"
+            result = "Error: Division by Zero!"
     st.write(f"Result: {result}")
