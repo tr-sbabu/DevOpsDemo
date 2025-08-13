50cd8f43088cf8c246edd9219096a4a757be23b4
tr-sbabu <contact.trsathishbabu@gmail.com>
2025-08-13 12:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ if st.button("Calculate"):
         if num2 != 0:
             result = num1 / num2
         else:
-            result = "Error: Division by Zero!"
+            result = "Error: Divide by Zero!"
     st.write(f"Result: {result}")
